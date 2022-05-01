56f5a7451d8d37428040fe777af52e3e4e9153c2
Lucas Ketzer <lucasketzerdosreis@gmail.com>
2022-05-01 00:54:08 -0300

    fix/feat: adding comments to exercise one and starting exercise 2

--- python.py
+++ python.py
@@ -1,16 +1,24 @@
-# Exercicio 1
+# Exercício 1
+# importing libraries
 import datetime as dt
 
 def datetime_to_excel(date: dt.datetime) -> int:
+    
+    '''Converts a datetime object to an int
+    that representes a serial excel date'''
 
     start_date = dt.datetime(1899, 12, 31)
 
     diff_to_start = date - start_date
-    
+
     return int(diff_to_start.days)
 
-def excel_to_datetime(date: int) -> dt.datetime:
 
+def excel_to_datetime(date: int) -> dt.datetime:
+    
+    '''Converts a serial excel date to a
+    datetime object'''
+    
     start_date = dt.datetime(1899, 12, 31)
 
     days_to_add = dt.timedelta(days = date)
@@ -19,9 +27,9 @@ def excel_to_datetime(date: int) -> dt.datetime:
 
     return datetime_date
 
+# validating results
+test_excel = datetime_to_excel(dt.datetime(2018, 1, 2))
+print(test_excel)
 
-teste_excel = datetime_to_excel(dt.datetime(2018, 1, 2))
-print(teste_excel)
-
-teste_datetime = excel_to_datetime(43101)
-print(teste_datetime)
+test_datetime = excel_to_datetime(43101)
+print(test_datetime)
--- r.R
+++ r.R
@@ -1,1 +1,23 @@
-# Exercicio 1
+# Exercicio 2
+
+# importing libraries
+library(tidyverse)
+library(vroom)
+
+# setting working directory
+wd <- getwd()
+
+# reading dataframe and renaming columns
+shares_df <- vroom(paste0(wd, "/tabela_acoes.csv")) %>%
+                dplyr::rename(
+                    ticker = `Ação`,
+                    client = Cliente,
+                    qtd = QTD,
+                    price = `PREÇO`
+                )
+
+# calculating total invested amount per client
+total_invested_per_client <- shares_df %>%
+                                mutate(invested_amount = price * qtd) %>%
+                                group_by(client) %>%
+                                summarise(total_invested_amount = sum(invested_amount))
